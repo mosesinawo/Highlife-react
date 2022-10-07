a50ed94aafa0db039521ec7aa647b3a5dc0b3d11
iambihan <inawo.moses@gmail.com>
2022-10-07 07:49:41 -0700

    highlife completed exept effect

--- src/components/highlife/Highlife.js
+++ src/components/highlife/Highlife.js
@@ -3,11 +3,16 @@ import { Col, Container, Row, } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Highlife.css';
 import niara1 from './img/niara-1.png'
+import tv1 from './img/tv-1.png';
+import tv2 from './img/tv-2.png';
+import tv3 from './img/tv-3.png';
+import Footer from '../../Footer.js/Footer';
 
 const Highlife = () => {
     return (
-        <Container fluid style={{ margin: ' ' }}>
+        <Container style={{ margin: ' ' }}>
             <Row className='highlife-black'>
+                
                 <Col md={8}>
                     <div>
 
@@ -15,9 +20,9 @@ const Highlife = () => {
                     </div>
                 </Col>
                 <Col md={4}>
-                    <div>
+                    <div style={{textAlign:'start', padding:'2rem 2rem 0 0'}}>
 
-                        <h2>Life on the Sweet Side</h2>
+                        <h2 style={{color:' #10d869', }}>Life on the Sweet Side</h2>
                         <h6>Carlton Williams</h6>
                         <p>Brody tries to protect his house as wildfires tear
                             through Malibu, and Justin is
@@ -26,7 +31,106 @@ const Highlife = () => {
                         </p>
                     </div>
                 </Col>
+            </Row>
 
+
+
+            <Row style={{marginTop:'10rem'}}>
+                <h1 className='highlife-header'>Other videos in this series:</h1>
+                <Col md={3}>
+                    <div className='highlife-container'>
+                        <div className='highlife-image'>
+                            <img src={niara1} className='highlife-img' alt=''/>
+                        </div>
+                        <div className='highlife-text'>
+                            <h5 className=''>Life on the sweet side 1</h5>
+                            <figcaption className='highlife-price'>#5000</figcaption>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={3}>
+                    <div className='highlife-container'>
+                        <div className='highlife-image'>
+                            <img src={niara1} className='highlife-img' alt=''/>
+                        </div>
+                        <div className='highlife-text'>
+                            <h5 className=''>Life on the sweet side 1</h5>
+                            <figcaption className='highlife-price'>#5000</figcaption>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={3}>
+                    <div className='highlife-container'>
+                        <div className='highlife-image'>
+                            <img src={niara1} className='highlife-img' alt=''/>
+                        </div>
+                        <div className='highlife-text'>
+                            <h5 className=''>Life on the sweet side 1</h5>
+                            <figcaption className='highlife-price'>#5000</figcaption>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={3}>
+                    <div className='highlife-container'>
+                        <div className='highlife-image'>
+                            <img src={niara1} className='highlife-img' alt=''/>
+                        </div>
+                        <div className='highlife-text'>
+                            <h5 className=''>Life on the sweet side 1</h5>
+                            <figcaption className='highlife-price'>#5000</figcaption>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+            <Row style={{marginTop:'10rem', marginBottom:'10rem'}}>
+                <h1 className='highlife-header'>You will also like:</h1>
+                <Col md={3}>
+                    <div className='highlife-container'>
+                        <div className='highlife-image'>
+                            <img src={niara1} className='highlife-img' alt=''/>
+                        </div>
+                        <div className='highlife-text'>
+                            <h5 className=''>Life on the sweet side 1</h5>
+                            <figcaption className='highlife-price'>#5000</figcaption>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={3}>
+                    <div className='highlife-container'>
+                        <div className='highlife-image'>
+                            <img src={tv1} className='highlife-img' alt=''/>
+                        </div>
+                        <div className='highlife-text'>
+                            <h5 className=''>Life on the sweet side 1</h5>
+                            <figcaption className='highlife-price'>#5000</figcaption>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={3}>
+                    <div className='highlife-container'>
+                        <div className='highlife-image'>
+                            <img src={tv2} className='highlife-img' alt=''/>
+                        </div>
+                        <div className='highlife-text'>
+                            <h5 className=''>Life on the sweet side 1</h5>
+                            <figcaption className='highlife-price'>#5000</figcaption>
+                        </div>
+                    </div>
+                </Col>
+                <Col md={3}>
+                    <div className='highlife-container'>
+                        <div className='highlife-image'>
+                            <img src={tv3} className='highlife-img' alt=''/>
+                        </div>
+                        <div className='highlife-text'>
+                            <h5 className=''>Life on the sweet side 1</h5>
+                            <figcaption className='highlife-price'>#5000</figcaption>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+            <Row>
+                <Footer/>
             </Row>
         </Container>
     )
